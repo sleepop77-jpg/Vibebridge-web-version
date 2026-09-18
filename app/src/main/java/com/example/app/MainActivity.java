acc5f882f8f226160ee42659ada125e84bb9634b
sleepop77-jpg <sleepop77@gmail.com>
2026-09-19 04:48:26 +0530

    feat: web push (1 ops)

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.app;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("Hello, VibeBridge!");
+        setContentView(tv);
+    }
+}
